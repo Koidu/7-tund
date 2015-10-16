1ae383850db5f8776a625e2d613827b72ece94c8
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2015-10-16 10:49:11 +0300

    failid

---
+++ functions.php
@@ -0,0 +1,61 @@
+﻿<?php
+
+	require_once("../configglobal.php");
+	$database = "if15_koidkan";
+	
+	// loome uue funktsiooni, et küsida andmeid
+	function getCarData(){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"],  $GLOBALS["server_password"],  $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
+		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
+		$stmt->execute();
+		
+		$row = 0;
+		
+		// tühi massiiv kus hoiame objekte edaspidi
+		$array = array();
+		
+		
+		while($stmt->fetch()){
+			
+			// loon objekti
+			$car = new StdClass();
+			$car->id = $id;
+			$car->number_plate = $number_plate;
+			$car->user_id = $user_id;
+			$car->color = $color_from_db;
+			
+			array_push($array, $car);
+			// echo "<pre>";
+			// var_dump($array);
+			// echo "</pre>";
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $array;
+		
+				
+		
+	}
+
+	function deleteCar($id_to_be_deleted){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"],  $GLOBALS["server_password"],  $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
+		
+		if($stmt->execute()){
+				header("Location: table.php");
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+	}
+
+?>
---
+++ table.php
@@ -0,0 +1,61 @@
+﻿<?php
+
+	require_once("functions.php");
+	
+	// kas kasutaja tahab kustutada
+	// kas aadressi real on ?delete=?????
+	if(isset($_GET["delete"])){
+		
+		deleteCar($_GET["delete"]);
+				
+	}
+
+	
+	
+	$car_list = getCarData();
+	// var_dump($car_list);
+
+?>
+<table border=1>
+	<tr> 
+		<th>id</th>
+		<th>auto nr märk</th>
+		<th>kasutaja id</th>
+		<th>värv</th>
+		<th>X</th>
+	</tr>
+	
+	<?php
+		for($i = 0; $i < count($car_list); $i++){
+			
+			// kui on see rida, mida kasutaja tahab muuta, siis kuvan inputi
+		if(isset($_GET["edit"])&& $car_list[$i]->id == $_GET["edit"]){
+				echo "<tr>";
+				echo "<form action='tablemethod='post'>";
+				echo "<td>".$car_list[$i]->id."</td>";
+				echo "<td><input name='number_plate' value='".$car_list[$i]->number_plate."'></td>";
+				echo "<td>".$car_list[$i]->user_id."</td>";
+				echo "<td><input name='color' value='".$car_list[$i]->color."'></td>";
+				echo "<td><input type='submit' name='update'></td>";
+				echo "<td><a href='table.php'>cancel</a></td>";
+				echo "</form>";
+				echo "</tr>";
+			}else{
+
+				echo "<tr>";
+			
+				echo "<td>".$car_list[$i]->id."</td>";
+				echo "<td>".$car_list[$i]->number_plate."</td>";
+				echo "<td>".$car_list[$i]->user_id."</td>";
+				echo "<td>".$car_list[$i]->color."</td>";
+				echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+				echo "<td><a href='?edit=".$car_list[$i]->id."'>edit</a></td>";
+			
+				echo" </tr>";
+			}
+		}	
+	?>
+	
+	
+	
+</table>
